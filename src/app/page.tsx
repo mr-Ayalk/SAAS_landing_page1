127bc85817099a801e51382d7d61b8e5106fbfe4
mr-Ayalk <ayalkteketel7@gmail.com>
2025-07-12 15:28:44 +0300

    footer section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import CallToAction from "@/sections/CallToAction";
 import Faqs from "@/sections/Faqs";
 import Features from "@/sections/Features";
+import Footer from "@/sections/Footer";
 import Hero from "@/sections/Hero";
 import Integrations from "@/sections/Integrations";
 import Introduction from "@/sections/Introduction";
@@ -17,7 +18,8 @@ export default function Home() {
             <Features />
             <Integrations />
             <Faqs />
-            <CallToAction/>
+            <CallToAction />
+            <Footer />
         </>
     );
 }
