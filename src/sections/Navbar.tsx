2a6dd6ad2ef7ee5e4d8aa148b74be69c4d8b312e
mr-Ayalk <ayalkteketel7@gmail.com>
2025-07-12 17:35:04 +0300

    navigation bar animation completed

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,81 +1,111 @@
+"use client";
 import Image from "next/image";
 import logoImage from "@/assets/images/logo.svg";
-import HambergerMenu from "@/assets/images/menu.svg";
 import Button from "@/components/Button";
-import { a } from "framer-motion/client";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 const navLinks = [
     { label: "Home", href: "#" },
     { label: "Features", href: "#features" },
     { label: "Integrations", href: "#integrations" },
     { label: "FAQs", href: "#faqs" },
 ];
+import { Menu, Plus } from "react-feather"; // Import the Menu icon
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <section className="py-4 lg:py-8">
-            <div className="container max-w-5xl">
-                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4  md:pr-2 items-center">
-                    <div className="">
-                        <Image
-                            src={logoImage}
-                            alt="Layers logo"
-                            className="h-9 w-auto md:h-auto  "
-                        />
-                    </div>
+        <>
+            <section className="py-4 lg:py-8 fixed w-full top-0 z-50">
+                <div className="container max-w-5xl ">
+                    <div className="border border-white/15 rounded-[27px] md:rounded-full bg-neutral-950/70 backdrop-blur">
+                        <div className="grid grid-cols-2 lg:grid-cols-3   p-2 px-4  md:pr-2 items-center ">
+                            <div className="">
+                                <Image
+                                    src={logoImage}
+                                    alt="Layers logo"
+                                    className="h-9 w-auto md:h-auto  "
+                                />
+                            </div>
 
-                    <div className="lg:flex  justify-center items-center hidden">
-                        <nav className="flex gap-6 font-medium">
-                            {navLinks.map((link) => (
-                                <a href={link.href} key={link.label}>
-                                    {link.label}
-                                </a>
-                            ))}
-                        </nav>
-                    </div>
+                            <div className="lg:flex  justify-center items-center hidden">
+                                <nav className="flex gap-6 font-medium">
+                                    {navLinks.map((link) => (
+                                        <a href={link.href} key={link.label}>
+                                            {link.label}
+                                        </a>
+                                    ))}
+                                </nav>
+                            </div>
 
-                    <div className="flex justify-end gap-4">
-                        {/* <Image
-                            src={HambergerMenu}
-                            alt=""
-                            width="24"
-                            height="24"
-                            className="text-white 
-                            bg-white  md:hidden
-                           "
-                        /> */}
+                            <div className="flex justify-end gap-4 ">
+                                <div
+                                    onClick={() => setIsOpen(!isOpen)}
+                                    style={{ cursor: "pointer" }}
+                                >
+                                    {!isOpen ? (
+                                        <Menu
+                                            size={24}
+                                            color="white"
+                                            className="md:hidden "
+                                        />
+                                    ) : (
+                                        <Plus
+                                            size={24}
+                                            color="white"
+                                            className="rotate-45 md:hidden"
+                                        />
+                                    )}
+                                </div>
 
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            className="feather feather-activity md:hidden"
-                        >
-                            <line x1="3" y1="12" x2="21" y2="12"></line>
-                            <line x1="3" y1="6" x2="21" y2="6"></line>
-                            <line x1="3" y1="18" x2="21" y2="18"></line>
-                        </svg>
-                        <Button
-                            variant="secondary"
-                            className="hidden md:inline-flex items-center"
-                        >
-                            Log In
-                        </Button>
+                                <Button
+                                    variant="secondary"
+                                    className="hidden md:inline-flex items-center"
+                                >
+                                    Log In
+                                </Button>
 
-                        <Button
-                            variant="primary"
-                            className="hidden md:inline-flex items-center"
-                        >
-                            Sign Up
-                        </Button>
+                                <Button
+                                    variant="primary"
+                                    className="hidden md:inline-flex items-center"
+                                >
+                                    Sign Up
+                                </Button>
+                            </div>
+                        </div>
+                        <AnimatePresence>
+                            {isOpen && (
+                                <motion.div
+                                    initial={{ height: 0 }}
+                                    animate={{ height: "auto" }}
+                                    exit={{ height: 0 }}
+                                    className=" overflow-hidden"
+                                >
+                                    <div className="flex flex-col items-center gap-4 py-4">
+                                        {navLinks.map((link) => (
+                                            <a
+                                                key={link.label}
+                                                href={link.href}
+                                                className=""
+                                            >
+                                                {link.label}
+                                            </a>
+                                        ))}
+
+                                        <Button variant="secondary">
+                                            Log In
+                                        </Button>
+                                        <Button variant="primary">
+                                            Sign Up
+                                        </Button>
+                                    </div>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <div className="pb-[86px] md:pb-[98px] lg:pb-[130px]"></div>
+        </>
     );
 }
