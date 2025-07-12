4030e9334c0f5c55efd3f3b4e0031d889ac5ec7a
mr-Ayalk <ayalkteketel7@gmail.com>
2025-07-12 22:12:53 +0300

    animation of integration section or page completed

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -68,6 +68,7 @@ export default function Integrations() {
                             <IntegrationColumn integrations={integrations} />
                             <IntegrationColumn
                                 integrations={integrations.slice().reverse()}
+                                reverse
                                 className="hidden md:flex"
                             />
                         </div>
